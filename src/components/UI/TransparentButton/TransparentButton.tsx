79eebb1a10732cdf4dbbe0476334076685e47e2c
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-04-26 20:47:42 +0700

    Transparent button styling

--- src/components/UI/TransparentButton/TransparentButton.tsx
+++ src/components/UI/TransparentButton/TransparentButton.tsx
@@ -1,4 +1,5 @@
 import React, { ButtonHTMLAttributes, FC } from 'react';
+import './transparent-button.scss';
 
 interface TransparentButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
