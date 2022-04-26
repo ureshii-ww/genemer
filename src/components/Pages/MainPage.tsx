9fe0178a47ab45504871feea7773e95e7824d9b7
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-04-26 19:52:12 +0700

    MainPage styling

--- src/components/Pages/MainPage.tsx
+++ src/components/Pages/MainPage.tsx
@@ -1,15 +1,22 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import GreetingsBlock from '../Containers/GreetingsBlock/GreetingsBlock';
 import MainForm from '../Containers/MainForm/MainForm';
 import StatusBlock from '../Containers/StatusBlock/StatusBlock';
+import './main-page.scss'
 
 const MainPage = () => {
   return (
-    <Fragment>
-      <GreetingsBlock />
-      <StatusBlock />
-      <MainForm />
-    </Fragment>
+    <div className="main-page">
+      <div className="main-page__container">
+        <header className="main-page__header">
+          <GreetingsBlock />
+          <StatusBlock />
+        </header>
+        <main className="main-page__content">
+          <MainForm />
+        </main>
+      </div>
+    </div>
   );
 };
 
