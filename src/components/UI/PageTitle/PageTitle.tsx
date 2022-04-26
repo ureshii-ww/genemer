e28a813aae637285bde7f46b88350429c67abe4c
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-04-26 13:20:15 +0700

    Added the PageTitle component

---
+++ src/components/UI/PageTitle/PageTitle.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+
+interface PageTitleProps {
+  greeting: string;
+  username: string;
+}
+
+const PageTitle: FC<PageTitleProps> = ({ greeting, username }) => {
+  const baseClass = 'page-title';
+
+  return (
+    <h1 className={baseClass}>
+      <span className={`${baseClass}__greeting`}>{greeting}</span>
+      <span className={`${baseClass}__username`}>{username}</span>
+    </h1>
+  );
+};
+
+export default PageTitle;
