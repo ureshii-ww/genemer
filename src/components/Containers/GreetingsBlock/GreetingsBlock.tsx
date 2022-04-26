e9bda68943590d279f6ed99483ea00486cc5ea2e
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-04-26 20:44:09 +0700

    Greetings block styling

--- src/components/Containers/GreetingsBlock/GreetingsBlock.tsx
+++ src/components/Containers/GreetingsBlock/GreetingsBlock.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PageTitle from '../../UI/PageTitle/PageTitle';
 import { useGreetingsBlock } from './useGreetingsBlock';
+import './greetings-block.scss'
 
 const GreetingsBlock = () => {
   const { username } = useGreetingsBlock();
 
   return (
-    <div>
+    <div className="greetings-block">
       <PageTitle greeting="Здравствуйте, " username={username} />
     </div>
   );
--- src/components/UI/PageTitle/PageTitle.tsx
+++ src/components/UI/PageTitle/PageTitle.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import './page-title.scss';
 
 interface PageTitleProps {
   greeting: string;
