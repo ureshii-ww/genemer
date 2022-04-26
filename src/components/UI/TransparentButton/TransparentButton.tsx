e163a52bf814c9dec053408d98b53a292fe1ae92
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-04-26 13:16:57 +0700

    Added the buttons components

---
+++ src/components/UI/TransparentButton/TransparentButton.tsx
@@ -0,0 +1,16 @@
+import React, { ButtonHTMLAttributes, FC } from 'react';
+
+interface TransparentButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const TransparentButton: FC<TransparentButtonProps> = ({ children, className, ...props }) => {
+  const baseClass = 'transparent-button';
+  const classString = className ? `${baseClass} ${className}` : baseClass;
+
+  return (
+    <button {...props} className={classString}>
+      {children}
+    </button>
+  );
+};
+
+export default TransparentButton;
