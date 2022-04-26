f698d86f9a4b3fb5e0fd89580cdbefd358245e3c
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-04-26 12:15:38 +0700

    Started the project

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import Layout from './components/Layout/Layout';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+    <Layout />
+);
